9c32f590026815b7564cfeaf80ed053690fd4dd8
manvote <manovate15@gmail.com>
2026-02-23 16:13:22 +0530

    Changes updated

---
+++ gunicorn_config.py
@@ -0,0 +1,38 @@
+# Gunicorn Configuration File
+
+import multiprocessing
+
+# Server socket
+bind = "127.0.0.1:8000"
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = "/var/www/dipanddash/logs/access.log"
+errorlog = "/var/www/dipanddash/logs/error.log"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
+
+# Process naming
+proc_name = "dipanddash"
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+keyfile = None
+certfile = None
+
+# Application
+pythonpath = "/var/www/dipanddash/food"
